62cd9e8a3cba9010ee5841cfd5d744950375fa37
Naww137 <57197000+Naww137@users.noreply.github.com>
2022-06-13 11:23:35 -0400

    Creation of sampling ability for chi-squared distribution

--- src/smpl_resparm/sample_widths.py
+++ src/smpl_resparm/sample_widths.py
@@ -10,10 +10,44 @@ Created on Fri Jun  3 11:08:47 2022
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import scipy.stats as stats
 
 
-def sample_resonance_widths():
+def sample_chisquare(N_samples, DOF):
+    samples = np.random.chisquare(DOF, size=N_samples)
+    return samples
     
-    do_stuff = 0
+def chisquare_PDF(x, DOF):
+    y = stats.chi2.pdf(x, DOF)
+    return y
 
-    return do_stuff
+def sample_resonance_widths(DOF, N_levels, avg_reduced_width_square):
+    
+    reduced_widths_square = avg_reduced_width_square*sample_chisquare(N_levels, DOF)
+    partial_widths = 0  # add function with penetrability =2*P(E)*red_wid_sqr
+    
+    return reduced_widths_square, partial_widths
+
+
+def compare_pdf_to_samples(reduced_widths_square_vector, avg_reduced_width_square, dof):
+    
+    fig = plt.figure(num=1,frameon=True); ax = fig.gca()
+    
+    x = np.linspace(0,max(reduced_widths_square_vector),10000)
+    plt.plot(x, chisquare_PDF(x,dof), color='r', label='$\chi^2$ PDF', zorder=10)
+    
+    if avg_reduced_width_square != 1:
+        print(); print('WARNING: ')
+        print('pdf has not been transformed for <D> other than 1 - will not match samples'); print()
+        
+    plt.hist(reduced_widths_square_vector, bins=75, density=True, ec='k', linewidth=0.75,color='pink', zorder=2, label='samples')
+    
+    ax.set_facecolor('whitesmoke'); ax.grid(color='w', linestyle='-', linewidth=2, zorder=1, alpha=1)
+    ax.set_xlabel('Reduced Widths Squared ($\gamma^2$)'); ax.set_ylabel('Normalized Frequency'); plt.title('Reduced Widths Squared ($\gamma^2$)')
+    plt.legend()
+    
+    return
+
+
+
+
