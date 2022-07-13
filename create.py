55ab2edc5c097414b8cbe56cb287de8e53428551
waltonna <nwalton1@vols.utk.edu>
2022-07-13 15:08:22 -0400

    Updates

--- create.py
+++ create.py
@@ -10,6 +10,8 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import syndat
+import scipy.stats as stat
 
 
 
@@ -68,24 +70,27 @@ def plot1(energy,theo,exp,label1,label2):
     plt.show();plt.close()
     
 
-def plot2(energy,theo,exp):
+def plot2(energy,theo,exp,exp_unc, title):
     
-    fig, (ax1,ax2) = plt.subplots(2,1, sharex=True, figsize=(5,5)) # , figsize=(12,5)
+    fig, (ax1,ax2) = plt.subplots(2,1, sharex=True, figsize=(6,4),gridspec_kw={'height_ratios': [2, 1]}) # , figsize=(12,5)
     plt.rcParams['figure.dpi'] = 500
     
-    ax1.plot(energy,theo, lw=0.5, label='$T_{theo}$')
-    ax1.scatter(energy,exp, s=0.1, c='r', label='$T_{exp}$')
+    ax1.plot(energy,theo, lw=0.5, color='b', label='$T_{theo}$', zorder=2)
+    #ax1.scatter(energy,exp, s=0.1, c='r', label='$T_{exp}$')
+    ax1.errorbar(energy, exp, yerr=exp_unc, color='k',ecolor='k',elinewidth=1,capsize=2, fmt='.', ms=3, label='$T_{exp}$', zorder=0)
     
     ax1.legend()
     ax1.set_ylabel('T') #('$\sigma$')
     #ax1.set_yscale('log'); 
     ax1.set_xscale('log')
+    ax1.set_ylim([0,max(exp)+0.1])
     
     rel_se = (exp-theo)/theo
     ax2.scatter(energy, rel_se, s=2)
-    ax2.set_ylim([-.1,.1])
-    ax2.set_xlabel('Energy'); ax2.set_ylabel('L1 Norm (relative)')
+    ax2.set_ylim([-.5,.5])
+    ax2.set_xlabel('ToF (s)'); ax2.set_ylabel('L1 Norm (relative)')
     
+    plt.suptitle(title)
     plt.tight_layout()
     plt.show(); plt.close()
 
@@ -114,83 +119,270 @@ energy = np.array(sammy_lst['E'])
 xs_theoretical = np.array(sammy_lst['theo_xs'])
 
 
+
 # =============================================================================
 #  simple uncertainty propagation from counts to transmission (no background)
 # =============================================================================
+
+# =============================================================================
+# experimentally unique values
+# =============================================================================
 n = .12
-time = 10
-flux_mag = 1e5
+trig = 10# number of linac pulses
+bw = 1e-3 # bin width
+flux_mag = 1e5 # what is a reasonable flux magnitude??
 detector_efficiency = 1
+tof_dist = 100 # m
+
+# assuming 1% uncertainty of these values
+m1 = 1; dm1 = m1*0.01
+m2 = 1; dm2 = m2*0.01
+m3 = 1; dm3 = m3*0.01
+m4 = 1; dm4 = m4*0.01
+alpha = [m1,m2,m3,m4]; d_alpha = [dm1,dm2,dm3,dm4]
+
+a = 1; da = a*0.05
+b = 1; db = b*0.05
+
+# sample in
+k = 0.1; dk = k*0.05
+b0 = 1; db0 = b0*0.05
+# sample out
+K = 0.1; dK = K*0.05
+B0 = 1; dB0 = B0*0.05
+
+tof = syndat.exp_effects.e_to_t(energy,tof_dist,True)
+
+# background function
+def bkg_func(ti,a,b):
+    return a*ti**-b
+Bi = bkg_func(tof,a,b)
 
 
-C = np.ones((len(energy)))*flux_mag
-seC = np.sqrt(C) # poisson counting statistics
+
+
+
+
+def generate_count_data(energy, xs_theo, flux_mag, bw, trig, n, Bi, k,K, b0,B0, alpha):
+    
+    [m1,m2,m3,m4] = alpha
+    
+    # =============================================================================
+    # open counts, sample out- do we want this to be noisey or clean here?
+    # =============================================================================
+    cts_o = stat.norm.pdf(energy, loc=50, scale=100)*flux_mag # gaussian in energy, std=range of energy
+    d_cts_o = np.sqrt(cts_o)
+    #ncts_o = gaus_noise(cts_o,d_cts_o) # noisey open counts
+    
+    # Plot open flux spectra
+    # =============================================================================
+    # fig, (ax1,ax2) = plt.subplots(2,1, gridspec_kw={'height_ratios': [1, 1]})
+    # ax1.plot(energy, C); ax1.set_xlabel('energy (eV)'); ax1.set_ylabel('C(E)')
+    # ax2.plot(tof,C); ax2.set_xlabel('tof (s)'); ax1.set_ylabel('C(t)')
+    # =============================================================================
+    
+    ctr_o = cts_o/(bw*trig)
+    #nctr_o = ncts_o/(bw*trig) # noisy, open count rate
+    
+    # =============================================================================
+    # find the number of counts given theoretical transmission
+    # =============================================================================
+    
+    trans_theo = np.exp(-n*xs_theo) # sammy can also just output this transmission value
+    
+    ctr_i = (trans_theo*(m3*ctr_o - m4*K*Bi - B0) + m2*k*Bi + b0)/m1
+    
+    cts_i = ctr_i*bw*trig 
+    d_cts_i = np.sqrt(cts_i)
+    ncts_i = gaus_noise(cts_i,d_cts_i)
+
+    return ncts_i
+
+
+
+
+
+
+# =============================================================================
+# now reduce the raw count data - can use the same, or different functions for C_open and background
+# =============================================================================
+
+
+
+# get noisey open count rate (sample out)
+cts_o = stat.norm.pdf(energy, loc=50, scale=100)*flux_mag # gaussian in energy, std=range of energy
+d_cts_o = np.sqrt(cts_o)
+ncts_o = gaus_noise(cts_o,d_cts_o) # noisey open counts
+
+
+ncrs_o = ncts_o/(bw*trig) # noisy open count rate
+
+# generate noisey count rate for sample in
+ncts_i = generate_count_data(energy,xs_theoretical, flux_mag,bw,trig, n, Bi, k,K, b0,B0, alpha)
+d_ncts_i = np.sqrt(ncts_i)
+
+
+
+
+def transmission(cr,Cr, Bi, k,K, b0,B0, alpha):
+    [m1,m2,m3,m4] = alpha
+    return (m1*c - m2*k*Bi - b0) / (m3*C - m4*K*Bi - B0) 
+
+crn = cn/(bw*trig)
+
+T = transmission(crn,Crn, Bi, k,K, b0,B0, alpha)
+
+
+
+
+# =============================================================================
+# sys_unc = np.append([da,db,dk,dK,db0,dB0], d_alpha)
+# 
+# CovT = syndat.exp_effects.get_covT(tof, Bi, dc,dC, sys_unc, a,b, k,K, c,C, b0,B0, alpha)
+# 
+# dT = np.diagonal(CovT)
+# 
+# plot2(energy,T,T_noise,dT, 'Fully Correlated Uncertainty')
+# =============================================================================
+
+
+
+
+#%%
+# flat flux for sample out
+C = np.ones((len(tof)))*flux_mag
+dC = np.sqrt(C) # poisson counting statistics
+C_noise = gaus_noise(C,dC)
 
 c = C*np.exp(-xs_theoretical*n) * detector_efficiency
-sec = np.sqrt(c) #poisson counting statistics
+dc = np.sqrt(c) #poisson counting statistics
+c_noise = gaus_noise(c,dc)
 
-T = c/C
-seT = np.sqrt((c/C**2) + (c**2/C**3))
+# need a proper function to convert to count rate and correct for deadtime
+# the function for transmission below assumes c/C are count rates
+# are these calculations included in the uncertainty propagation? technically the poisson unc is on the raw count #
+# Cr = C/bin_width
+# cr = c/bin_width
 
-Texp = gaus_noise(T,seT)
 
-plot2(energy, T, Texp)
+# calculate noisey transmission points and background 
+def bkg_func(ti,a,b):
+    return a*ti**-b
+
+def transmission(alpha, c,C, k,K,  Bi, b0,B0):
+    return (alpha[0]*c - alpha[1]*k*Bi - b0) / (alpha[2]*C - alpha[3]*K*Bi - B0) 
+ 
+Bi = bkg_func(tof,a,b)
+
+T = transmission(alpha, c,C, k,K, Bi, b0,B0)
+T_noise = transmission(alpha, c_noise,C_noise, k,K, Bi, b0,B0)
+
+
+# =============================================================================
+# uncertainty propagation from poisson uncertainty on count rates
+# =============================================================================
+
+
+
+
+
+sys_unc = np.append([da,db,dk,dK,db0,dB0], dalpha)
+
+CovT = syndat.exp_effects.get_covT(tof, Bi, dc,dC, sys_unc, a,b, k,K, c,C, b0,B0, alpha)
+
+dT = np.diagonal(CovT)
+
+plot2(energy,T,T_noise,dT, 'Fully Correlated Uncertainty')
+
+
+inv_dT = np.linalg.inv(np.sqrt(np.diag(dT)))
+CorrT = inv_dT @ CovT @ inv_dT
+plt.matshow(CorrT)
+cb = plt.colorbar()
+#cb.ax.tick_params(labelsize=10)
+plt.title('Correlation of Transmission', fontsize=10);
+plt.show(); plt.close()
 
 
 #%%
 # =============================================================================
-# rather than propagating uncertainty to T, I'm going to build a covariance matrix
-# and sample from it s.t. the propagted uncertainties will be correlated with one another
-# also, this will be done for the cross section sigmat because the cov derivation is greatly simplified by the ln()
+# Covariance matrix from ALEX manual
 #   - derivation is done for channel to channel, when in actuality each data point I am looking at is a sum over multiple channels
 #   - interesting how dc and dC don't show up, is this correct? see ALEX manual
 #       - this is because dc = sqrt(c) and dc only shows up as dc^2 -> c
 #   - in the ALEX manual, why is there not a subscript i bor the background spectra?
 # =============================================================================
 
-def var_sig_i(sigi, c, C, d, D, n,dn, m,dm, M,dM, g,dg, G,dG):
-    var = 1/n * ( (sigi*dn**2) + (dM/M)**2 + (dm/m)**2 \
-                 + (dg/(d*c-g))**2 + (dG/(D*C-G))**2 \
-                 + d**2*c/(d*c-g)**2 + D**2*C/(D*C-G)**2 )
-    return var
-    
-def cov_sig_ij(sigi, sigj, di,ci,Di,Ci, dj,cj,Dj,Cj, n,dn, M,dM, m,dm, g,dg, G,dG):
-    cov_ij = 1/n**2 * ( sigi*sigj*dn**2 + (dM/M)**2 + (dm/m)**2 \
-                       + dg**2/((di*ci-g)*(dj*cj-g)) \
-                       + dG**2/((Di*Ci-G)*(Dj*Cj-G)) )
-    return cov_ij
-        
-energy = np.linspace(1,100,10)
-dn=0
-m = 1; dm = 0
-M = 1; dM = 0 
-g = 1; dg = .4
-G = 1; dG = .4
+# =============================================================================
+# def var_sig_i(sigi, c, C, d, D, n,dn, m,dm, M,dM, g,dg, G,dG):
+#     var = 1/n * ( (sigi*dn**2) + (dM/M)**2 + (dm/m)**2 \
+#                  + (dg/(d*c-g))**2 + (dG/(D*C-G))**2 \
+#                  + d**2*c/(d*c-g)**2 + D**2*C/(D*C-G)**2 )
+#     return var
+#     
+# def cov_sig_ij(sigi, sigj, di,ci,Di,Ci, dj,cj,Dj,Cj, n,dn, M,dM, m,dm, g,dg, G,dG):
+#     cov_ij = 1/n**2 * ( sigi*sigj*dn**2 + (dM/M)**2 + (dm/m)**2 \
+#                        + dg**2/((di*ci-g)*(dj*cj-g)) \
+#                        + dG**2/((Di*Ci-G)*(Dj*Cj-G)) )
+#     return cov_ij
+#         
+# energy = np.linspace(1,100,10)
+# dn=0
+# m = 1; dm = 0
+# M = 1; dM = 0 
+# g = 1; dg = .4
+# G = 1; dG = .4
+# 
+# d = np.ones([len(energy)])
+# D = np.ones([len(energy)])
+# 
+# n = .12
+# time = 10
+# flux_mag = 1e4
+# detector_efficiency = 1
+# 
+# 
+# C = np.ones((len(energy)))*flux_mag
+# seC = np.sqrt(C) # poisson counting statistics
+# C_noise = gaus_noise(C,seC)
+# 
+# c = C*np.exp(-xs_theoretical*n) * detector_efficiency
+# sec = np.sqrt(c) #poisson counting statistics
+# c_noise = gaus_noise(c,sec)
+# 
+# 
+# 
+# covmat = np.zeros([len(energy),len(energy)])
+# for i in range(len(energy)):
+#     for j in range(len(energy)):
+#         
+#         if i == j:
+#             covmat[i,j] = var_sig_i(xs_theoretical[i],c[i],C[i],d[i],D[i], \
+#                                     n,dn, m,dm, M,dM, g,dg, G,dG)
+#         else:
+#             covmat[i,j] = cov_sig_ij(xs_theoretical[i], xs_theoretical[j], \
+#                                      d[i],c[i],D[i],C[i], \
+#                                      d[j],c[j],D[j],C[j], \
+#                                      n,dn, M,dM, m,dm, g,dg, G,dG)
+#                 
+#                 
+# T = M*(d*c-g)/m*(D*C-G)
+# T_noise = M*(d*c_noise-g)/m*(D*C_noise-G)
+# 
+# seT = np.sqrt((c/C**2) + (c**2/C**3))
+# 
+# Texp = gaus_noise(T,seT)
+# 
+# plot2(energy, T, T_noise)
+# =============================================================================
 
-d = np.ones([len(energy)])
-D = np.ones([len(energy)])
-
-
-covmat = np.zeros([len(energy),len(energy)])
-for i in range(len(energy)):
-    for j in range(len(energy)):
-        
-        if i == j:
-            covmat[i,j] = var_sig_i(xs_theoretical[i],c[i],C[i],d[i],D[i], \
-                                    n,dn, m,dm, M,dM, g,dg, G,dG)
-        else:
-            covmat[i,j] = cov_sig_ij(xs_theoretical[i], xs_theoretical[j], \
-                                     d[i],c[i],D[i],C[i], \
-                                     d[j],c[j],D[j],C[j], \
-                                     n,dn, M,dM, m,dm, g,dg, G,dG)
-                
-                
-
-covL = np.tril(covmat)
-uncor_unitvar = np.random.default_rng().normal(loc=0.0, scale=1, size=len(energy))
-
-test = np.dot(uncor_unitvar, uncor_unitvar.T)
-
+# =============================================================================
+# to sample a covariance matrix, but I don't think we want to do this
+# covL = np.tril(covmat)
+# uncor_unitvar = np.random.default_rng().normal(loc=0.0, scale=1, size=len(energy))
+# 
+# test = np.dot(uncor_unitvar, uncor_unitvar.T)
+# =============================================================================
 
 
 
--- syndat/exp_effects.py
+++ syndat/exp_effects.py
@@ -34,6 +34,116 @@ def e_to_t(E,d,rel):
     return t
 
 
+def get_covT(tof, Bi, dc,dC, sys_unc, a,b, k,K, c,C, b0,B0, alpha):
+    
+    """
+    Calculates the output covariance matrix of transmission from input uncertainties.
+
+    This function uses the covariance sandwhich rule:
+    .. math:: C_y = J^T*C_x*J
+    to propagate input variance-covariance to transmission data
+
+    Parameters
+    ----------
+    tof : array-like
+        Array of time of flight values for each data point - corresponds to energy.
+    Bi : float or int
+        Time dependent background shape function.
+    dc : float
+        Uncertainty in the count rate for sample-in.
+    dC : array-like
+        Uncertainty in the count rate for sample-out.
+    Davg : array-like
+        Nested list of average level spacing for each spin group number. First 
+        list is for negative parity (J-) second is for positive parity (J+).
+    Gavg : array-like
+        Nested list of average widths for each spin group number. First 
+        list is for negative parity (J-) second is for positive parity (J+).
+    Gavg_swave : float
+        Average width used to sample agregate capture widths. **Unsure of this value.
+    print_out : bool
+        User option to print out quantum spin (J) mapping to console.
+    save_csv : bool
+        User option to save resonance ladders to csv.
+    sammy_run_folder : str
+        Folder in which the csv(s) containing resparm ladders will be saved.
+
+    Notes
+    -----
+    Unsure of the average capture width for Gg sampling.
+    
+    Returns
+    -------
+    Jn_df : DataFrame
+        Pandas DataFrame conatining a resonance parameter ladder for each 
+        quantum spin group with negative parity (all J-).
+    Jp_df : DataFrame
+        Pandas DataFrame conatining a resonance parameter ladder for each 
+        quantum spin group with positive parity (all J+).
+    """
+
+    # derivatives
+    D = alpha[2]*C - alpha[3]*K*Bi - B0
+    N = alpha[0]*c - alpha[1]*k*Bi - b0
+    
+    def dTi_dci(i):
+        return alpha[0]/D[i]
+    def dTi_dCi(i):
+        return N[i]*alpha[2]/D[i]
+    def dT_dsys(i):
+        dTi_da = -(k*alpha[1]*D[i]+K*alpha[3]*N[i])*np.exp(-b*tof[i]) / (D[i]**2)
+        dTi_db = (k*alpha[1]*D[i])*Bi[i]*tof[i] / (D[i]**2)
+        dTi_dk = -alpha[1]*Bi[i]/D[i]**2
+        dTi_dK = -N[i]*alpha[3]*Bi[i]/D[i]**2
+        dTi_db0 = -1/D[i]
+        dTi_dB0 = N[i]/D[i]**2
+        dTi_dalpha = [ c[i]/D[i], -k*Bi[i]/D[i], -C[i]*N[i]/D[i]**2, K*Bi[i]*N[i]/D[i]**2 ]
+        return np.append([dTi_da, dTi_db, dTi_dk, dTi_dK, dTi_db0, dTi_dB0], dTi_dalpha)
+    
+    # construct statistical covariance and jacobian
+    Cov_stat = np.zeros([len(tof*2),len(tof*2)])
+    #samplein = True; sampleout = False
+    for i in range(len(tof)):
+        for j in range(len(tof)):
+            if i == j:
+                Cov_stat[i,j] = dc[i] 
+    for i in range(len(tof),len(tof*2)):
+        for j in range(len(tof),len(tof*2)):
+            if i == j:
+                Cov_stat[i,j] = dC[i]
+    
+    Jac_stat = np.zeros([len(tof*2),len(tof*2)])
+    for i in range(len(tof)):
+        for j in range(len(tof)):
+            if i == j:
+                Jac_stat[i,j] = dTi_dci(i)
+    for i in range(len(tof),len(tof*2)):
+        for j in range(len(tof),len(tof*2)):
+            if i == j:
+                Jac_stat[i,j] = dTi_dCi(i)
+    
+    # construct systematic covariance and jacobian
+
+    Cov_sys = np.zeros([len(sys_unc),len(sys_unc)])
+    for i in range(len(sys_unc)):
+        for j in range(len(sys_unc)):
+            if i == j:
+                Cov_sys[i,j] = sys_unc[i]
+    Cov_sys[0,1] = sys_unc[0]*sys_unc[1]  
+    Cov_sys[1,0] = sys_unc[1]*sys_unc[0]        
+    
+    Jac_sys = np.zeros([len(sys_unc),len(tof)])
+    for i in range(len(sys_unc)):
+        for j in range(len(tof)):
+            Jac_sys[i,j] = dT_dsys(j)[i]
+                
+    # calculate covariance of output
+    CovT_stat = Jac_stat.T @ Cov_stat @ Jac_stat
+    CovT_sys = Jac_sys.T @ Cov_sys @ Jac_sys
+    
+    CovT = CovT_stat + CovT_sys
+    
+    return CovT
 
 
 
