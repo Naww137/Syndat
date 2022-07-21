027b16fb32023533efe42b2f527ef0af7e2692b3
waltonna <nwalton1@vols.utk.edu>
2022-07-21 12:36:11 -0400

    Updates

--- examples_exp.py
+++ examples_exp.py
@@ -114,7 +114,7 @@ C_o = pd.read_csv(os.path.join(sammy_directory,'ta181opencountrate.dat'), sep=',
 C_o['E'] = pd.read_csv(os.path.join(sammy_directory,'ta-181-12mm.twenty'), sep='\s+', names=['E','1','2'])['E']
 
 ctr_o_true = C_o['co'][0:-5] # C_o.loc[C_o['E'] < 330, 'co']
-tof = C_o['tof'][0:-5]*1e-6 # C_o.loc[C_o['E'] < 330, 'tof'] *1e-6
+tof = C_o['tof'][0:-5] # C_o.loc[C_o['E'] < 330, 'tof'] *1e-6
 energy = C_o['E'][0:-5]
 
 #plt.plot(C_o['tof'], C_o['co'])
@@ -256,36 +256,36 @@ Tn = syndat.exp_effects.transmission(ctr_i,ctr_o, Bi, k_i,K_o, b0_i,B0_o, alpha)
 #%% plot generated counts and count rate for sample in/sample out
 
 
+plt.scatter(tof, cts_i, s=1, label='c_s')
+plt.scatter(tof, cts_o, s=1, label='c_o')
+#plt.plot(tof, T_theo, label='trans', c='k')
+#plt.plot(tof, Bi, label='Bi', c='k')
+
+#plt.legend()
+#plt.xlim([1e-4,1e-3]);
+
+#plt.xscale('log'); plt.yscale('log')
+
+#plt.title('Count comparison with overlayed theoretical transmission')
+
+plt.scatter(tof, ctr_i, s=1, label='cr_s')
+plt.scatter(tof, ctr_o, s=1, label='cr_o')
+plt.plot(tof, T_theo, label='Theoretical T', c='k', alpha=0.5)
+
+plt.legend()
+plt.ylim([1e-2,1e5])
+plt.xlim([2e2, 2.5e2])
 # =============================================================================
-# plt.scatter(tof, cts_i, s=1, label='c_s')
-# plt.scatter(tof, cts_o, s=1, label='c_o')
-# #plt.plot(tof, T_theo, label='trans', c='k')
-# #plt.plot(tof, Bi, label='Bi', c='k')
-# 
-# plt.legend()
-# #plt.xlim([1e-4,1e-3]);
-# plt.ylim([1e-2,1e7])
-# plt.xscale('log'); plt.yscale('log')
-# 
-# #plt.title('Count comparison with overlayed theoretical transmission')
-# 
-# plt.scatter(tof, ctr_i, s=1, label='cr_s')
-# plt.scatter(tof, ctr_o, s=1, label='cr_o')
-# plt.plot(tof, T_theo, label='Theoretical T', c='k', alpha=0.5)
-# 
-# plt.legend()
-# # =============================================================================
-# # plt.xlim([1.5e-4,2.1e-4]);
-# # plt.ylim([4e2,1e3])
-# # =============================================================================
-# plt.xscale('log'); plt.yscale('log')
-# 
-# plt.title('Count Rate comparison with overlayed theoretical transmission')
-# plt.show(); plt.close()
-# 
-# 
-# #plt.plot(tof,cts_o_true*np.exp(-n*xs_theoretical))
+# plt.xlim([1.5e-4,2.1e-4]);
+# plt.ylim([4e2,1e3])
 # =============================================================================
+plt.xscale('log'); plt.yscale('log')
+
+plt.title('Count Rate comparison with overlayed theoretical transmission')
+plt.show(); plt.close()
+
+
+#plt.plot(tof,cts_o_true*np.exp(-n*xs_theoretical))
 
     
     
@@ -337,200 +337,71 @@ sys_unc = np.append([da,db,dk_i,dK_o,db0_i,dB0_o], d_alpha)
 Tn, dT, CovT = syndat.exp_effects.reduce_raw_count_data(tof, noisy_cts_i,noisy_cts_o, noisy_cts_i_se,noisy_cts_o_se, \
                                                         bw, trig, a,b, k_i,K_o, Bi, b0_i,B0_o, alpha, sys_unc)
 
-
+CorT = np.corrcoef(CovT)
     
 #%%
 
 
-plt.scatter(tof, Tn, label='Experimental', s=1, c='k')
-plt.plot(tof, T_theo, label='Theoretical', c='g', lw=0.25)
 
-plt.legend()
-plt.ylim([1e-3,1e1])
-plt.xscale('log');plt.yscale('log')
+syndat.plot.exp_theo(tof, Tn, dT, T_theo)
+    
+syndat.plot.unc_noise(tof, dT, T_theo, Tn)
 
 #%%
 
-fig, ax = plt.subplots(2,2, gridspec_kw={'height_ratios': [1, 1], 'width_ratios':[2,1]}) # , figsize=(12,5)
-plt.rcParams['figure.dpi'] = 500
-ax1 = ax[0,0]; ax2=ax[1,0]; ax3=ax[0,1]; ax4=ax[1,1]
+plt.matshow(CorT)
+ax = plt.gca()
 
-ax1.scatter(tof, dT, lw=0.5, color='b', s=2, zorder=2)
-ax1.set_ylim([0,10])
-ax1.set_ylabel('$\delta$T'); ax1.set_xscale('log')#('$\sigma$')
+ax.set_xticklabels(round(ax.get_xticks()), rotation = 45)
+cb = plt.colorbar()
+plt.title('Correlation')
 
-rel_se = (Tn-T_theo)/T_theo
-ax2.scatter(tof, (Tn-T_theo), s=2)
-ax2.set_xlabel('ToF (s)'); ax2.set_ylabel('Noise')
-
-# =============================================================================
-# ax3.matshow(CovT)
-# ax4.matshow(np.corrcoef(CovT))
-# =============================================================================
-plt.xscale('log')
-
-plt.suptitle('Uncertainty and Noise on Transmission')
-plt.tight_layout()
 plt.show(); plt.close()
 
 
 
 
+#%%
 
 
+T_df = pd.DataFrame(data=np.array([tof,Tn,dT]).T, columns=['tof', 'Tn', 'dT'])
 
+T_df['E'] = energy # syndat.exp_effects.t_to_e(T_df['tof'], tof_dist, False)
 
+#%% look at a window
 
+co_notch_index = np.where(ctr_o_true == min(ctr_o_true))
 
-
-
-
-#%% Ignore below this: came from previous work, incorrect method
-
-
+# find RRR above co notch
 # =============================================================================
-# Covariance matrix from ALEX manual
-#   - derivation is done for channel to channel, when in actuality each data point I am looking at is a sum over multiple channels
-#   - interesting how dc and dC don't show up, is this correct? see ALEX manual
-#       - this is because dc = sqrt(c) and dc only shows up as dc^2 -> c
-#   - in the ALEX manual, why is there not a subscript i bor the background spectra?
-# =============================================================================
-
-# =============================================================================
-# def var_sig_i(sigi, c, C, d, D, n,dn, m,dm, M,dM, g,dg, G,dG):
-#     var = 1/n * ( (sigi*dn**2) + (dM/M)**2 + (dm/m)**2 \
-#                  + (dg/(d*c-g))**2 + (dG/(D*C-G))**2 \
-#                  + d**2*c/(d*c-g)**2 + D**2*C/(D*C-G)**2 )
-#     return var
-#     
-# def cov_sig_ij(sigi, sigj, di,ci,Di,Ci, dj,cj,Dj,Cj, n,dn, M,dM, m,dm, g,dg, G,dG):
-#     cov_ij = 1/n**2 * ( sigi*sigj*dn**2 + (dM/M)**2 + (dm/m)**2 \
-#                        + dg**2/((di*ci-g)*(dj*cj-g)) \
-#                        + dG**2/((Di*Ci-G)*(Dj*Cj-G)) )
-#     return cov_ij
-#         
-# energy = np.linspace(1,100,10)
-# dn=0
-# m = 1; dm = 0
-# M = 1; dM = 0 
-# g = 1; dg = .4
-# G = 1; dG = .4
-# 
-# d = np.ones([len(energy)])
-# D = np.ones([len(energy)])
-# 
-# n = .12
-# time = 10
-# flux_mag = 1e4
-# detector_efficiency = 1
-# 
-# 
-# C = np.ones((len(energy)))*flux_mag
-# seC = np.sqrt(C) # poisson counting statistics
-# C_noise = gaus_noise(C,seC)
-# 
-# c = C*np.exp(-xs_theoretical*n) * detector_efficiency
-# sec = np.sqrt(c) #poisson counting statistics
-# c_noise = gaus_noise(c,sec)
-# 
-# 
-# 
-# covmat = np.zeros([len(energy),len(energy)])
-# for i in range(len(energy)):
-#     for j in range(len(energy)):
-#         
-#         if i == j:
-#             covmat[i,j] = var_sig_i(xs_theoretical[i],c[i],C[i],d[i],D[i], \
-#                                     n,dn, m,dm, M,dM, g,dg, G,dG)
-#         else:
-#             covmat[i,j] = cov_sig_ij(xs_theoretical[i], xs_theoretical[j], \
-#                                      d[i],c[i],D[i],C[i], \
-#                                      d[j],c[j],D[j],C[j], \
-#                                      n,dn, M,dM, m,dm, g,dg, G,dG)
-#                 
-#                 
-# T = M*(d*c-g)/m*(D*C-G)
-# T_noise = M*(d*c_noise-g)/m*(D*C_noise-G)
-# 
-# seT = np.sqrt((c/C**2) + (c**2/C**3))
-# 
-# Texp = gaus_noise(T,seT)
-# 
-# plot2(energy, T, T_noise)
-# =============================================================================
-
-# =============================================================================
-# to sample a covariance matrix, but I don't think we want to do this
-# covL = np.tril(covmat)
-# uncor_unitvar = np.random.default_rng().normal(loc=0.0, scale=1, size=len(energy))
-# 
-# test = np.dot(uncor_unitvar, uncor_unitvar.T)
+# emin = 132
+# emax = 330
+# Ew = T_df.loc[(emax['E']<330) & (T_df['E']>emin), 'E']
+# Tw = T_df.loc[(emax['E']<330) & (T_df['E']>emin ), 'Tn']
+# dTw = T_df.loc[(emax['E']<330) & (T_df['E']>emin ), 'dT']
+# tofw = T_df.loc[(emax['E']<330) & (T_df['E']>emin ), 'tof']
 # =============================================================================
 
 
+maxdT_index = np.where(dT==max(dT))[0][0]
+min_ind = maxdT_index-200
+max_ind = maxdT_index+200
+
+tofw = tof[min_ind:max_ind]
+Tw = Tn[min_ind:max_ind]
+dTw = dT[min_ind:max_ind]
+T_theow = T_theo[min_ind:max_ind]
 
 #%%
-# =============================================================================
-# 
-# xs_experimental = gaus_noise(xs_theoretical,1e-2)
-# 
-# fig, (ax1,ax2) = plt.subplots(2,1, sharex=True, figsize=(5,5)) # , figsize=(12,5)
-# plt.rcParams['figure.dpi'] = 500
-# 
-# 
-# ax1.plot(energy,xs_theoretical, lw=0.5, label='$\sigma_{theo}$')
-# ax1.scatter(energy,xs_experimental, s=0.1, c='r', label='$\sigma_{exp}$')
-# 
-# ax1.legend()
-# ax1.set_ylabel('$\sigma$')
-# ax1.set_yscale('log'); ax1.set_xscale('log')
-# 
-# 
-# 
-# ax2.scatter(energy, (xs_experimental-xs_theoretical), s=2)
-# ax2.set_xlabel('Energy'); ax2.set_ylabel('L1 Norm')
-# 
-# plt.tight_layout()
-# plt.show(); plt.close()
-# 
-# 
-# 
-# # if you provide a more course energy grid, you can resolve the structure of the cov/corr better
-# #energy = np.linspace(0.1,100,50)
-# 
-# 
-# # very simple background function from sammy
-# def bkg_func(E, a,b,c,d,f):
-#     return a + b/np.sqrt(E) + c*np.sqrt(E) + d*np.exp(-f/np.sqrt(E))
-# 
-# 
-# #plt.plot(energy, bkg_func(energy, 0,10,0.2,10,1))
-# 
-# background = bkg_func(energy, 0,10,0.2,10,1)
-# plt.plot(energy, background)
-# plt.title('Background Function')
-# plt.show(); plt.close()
-# 
-# def repeat_function(vector):
-#     return np.repeat([vector,vector],len(vector)*2,axis=0)
-# 
-# 
-# # if you make this perfectly correlated by just repeating the background function and taking corr, 
-# # you get a perfectly 1:1 correlation between all values - is this what we want?
-# # or do we want to sample noise around the background function? 
-# #bkg_cov = np.corrcoef(repeat_function(background))
-# 
-# random_bkg = [gaus_noise(bkg_sample, 0.01) for bkg_sample in repeat_function(background)]
-# bkg_cov = np.cov(random_bkg)
-# 
-# plt.matshow(bkg_cov)
-# cb = plt.colorbar()
-# cb.ax.tick_params(labelsize=10)
-# plt.title('Cov of bkg function', fontsize=10);
-# 
-# plt.show(); plt.close()
-# 
-# =============================================================================
+
+syndat.plot.unc_noise(tofw, dTw, T_theow, Tw)
+syndat.plot.exp_theo(tofw, Tw, dTw, T_theow)
+
+#%%
+plt.errorbar(tofw, Tw, yerr=dTw, label='T', color='r',ecolor='k',elinewidth=1,capsize=2, fmt='.', ms=3)
+plt.xscale('log'); plt.yscale('log')
+plt.show(); plt.close()
+
 
 
 
